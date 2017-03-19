011957af825547ae171c6755c2155398c7bae62a
Elliot Bonneville <elliot.bonneville@gmail.com>
2017-03-19 17:11:12 -0400

    Clean up layout file

--- client/src/js/layouts/CoreLayout/CoreLayout.js
+++ client/src/js/layouts/CoreLayout/CoreLayout.js
@@ -2,31 +2,17 @@ import React, { PropTypes } from 'react';
 import { Jumbotron } from 'react-bootstrap';
 
 export default function CoreLayout(props) {
-    let children = null;
-    if (props.children) {
-        // sends auth instance to children
-        children = React.cloneElement(props.children, {
-            auth: props.route.auth,
-        });
-    }
-
     return (
         <Jumbotron>
-            {children}
+            {props.children}
         </Jumbotron>
     );
 }
 
-CoreLayout.contextTypes = {
-    router: PropTypes.object,
-};
-
 CoreLayout.propTypes = {
     children: PropTypes.object,
-    route: PropTypes.object,
 };
 
 CoreLayout.defaultProps = {
     children: {},
-    route: {},
 };
