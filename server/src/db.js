076a19f8c189af6526c62514811c760b63e53ca8
Elliot Bonneville <elliot.bonneville@gmail.com>
2017-03-20 19:42:39 -0400

    Refactor database queries to use pool so connection is preserved

--- server/src/db.js
+++ server/src/db.js
@@ -1,19 +1,24 @@
 import mysql from 'mysql';
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: 'oxmind.cuypuqkjhqol.us-east-2.rds.amazonaws.com',
     user: 'oxbowdev',
     password: 'Ysnp!1955',
     database: 'oxmind_db',
 });
 
-connection.connect();
-
 export function query(queryString) {
     return new Promise((resolve, reject) => {
-        connection.query(queryString, (error, results) => {
-            if (error) reject(error);
-            else resolve(results);
+        pool.getConnection((connectionError, connection) => {
+            if (connectionError) {
+                reject(connectionError);
+            } else {
+                connection.query(queryString, (queryError, results) => {
+                    connection.release();
+                    if (queryError) reject(queryError);
+                    else resolve(results);
+                });
+            }
         });
     });
 }
