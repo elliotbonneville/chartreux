ca331cd094530f846d6ead01b0ddce2a3ae58276
Elliot Bonneville <elliot.bonneville@gmail.com>
2017-03-20 22:20:07 -0400

    Make projects list narrower

--- client/src/js/layouts/CoreLayout/CoreLayout.js
+++ client/src/js/layouts/CoreLayout/CoreLayout.js
@@ -1,11 +1,9 @@
 import React, { PropTypes } from 'react';
-import { Link } from 'react-router';
 import { Grid } from 'react-bootstrap';
 
 export default function CoreLayout(props) {
     return (
         <Grid>
-            <Link to="/projects/">Home</Link>
             {props.children}
         </Grid>
     );
--- client/src/js/routes/Projects/components/ProjectsView.js
+++ client/src/js/routes/Projects/components/ProjectsView.js
@@ -4,21 +4,27 @@ import {
     Button,
     ListGroup,
     ListGroupItem,
+    Row,
+    Col,
 } from 'react-bootstrap';
 
 export default function ProjectsView(props) {
     const { projects } = props;
     return (
         <div>
-            <h2>Projects</h2>
-            <ListGroup>
-                {projects.map(project =>
-                    <ListGroupItem key={project.id}>
-                        <Link to={`/targets/${project.id}`}>{project.project}</Link>
-                    </ListGroupItem>,
-                )}
-            </ListGroup>
-            <Button onClick={props.logout}>Logout</Button>
+            <Row>
+                <Col xs={6} md={3}>
+                    <h2>Projects</h2>
+                    <ListGroup>
+                        {projects.map(project =>
+                            <ListGroupItem key={project.id}>
+                                <Link to={`/targets/${project.id}`}>{project.project}</Link>
+                            </ListGroupItem>,
+                        )}
+                    </ListGroup>
+                    <Button onClick={props.logout}>Logout</Button>
+                </Col>
+            </Row>
         </div>
     );
 }
